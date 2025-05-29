c8d6e69ead49ef70adeb5e57c20a68f591557254
Izzatul Najwa binti Ismail <inbi1@student.london.ac.uk>
2025-05-30 02:38:26 +0800

    commit the ones above 20 changes

--- bash_test.sh
+++ bash_test.sh
@@ -1,10 +1,50 @@
-git diff --stat --ignore-cr-at-eol . | awk 'BEGIN{init = 1; flag = 0} {print "test diff", $0,"\n"}' > test_diff.txt
-git log --numstat --pretty=oneline -w --ignore-cr-at-eol --ignore-blank-lines  | awk 'BEGIN{init = 1; flag = 0} 
-{if (init==0 && $1 ~ /^[0-9]+$/ && ($1>20 || $2>20)) 
-    {print "too many insertions (",$1,") or deletions (",$2,") in file ", $3; flag=1}
-}  
-{if (length($1) == 40) 
-    {init=0; if (flag==1) 
-        {print "in commit",$0,"\n"; flag =0}
+RED="\033[31m"
+GREEN="\033[32m"
+NC='\033[0m'
+
+changes=($(git diff --stat --ignore-cr-at-eol .| awk '
+{if ($1 !~ /^[0-9]/) 
+    {print int($3);}
+}
+'))
+
+git diff --stat --ignore-cr-at-eol .| awk '
+{if ($1 !~ /^[0-9]/) 
+    {if ($3>20)
+        {print "\033[31m" $0 "\033[0m"}
+    else
+        {print "\033[32m" $0 "\033[0m"}
     }
-}' > abnormal-log.txt
+}
+'
+
+echo ""
+
+max=0
+
+for i in "${changes[@]}"
+do 
+    if [ $i -ge $max ]; then
+        max="$i"
+    fi
+
+done
+
+if [ $max -ge 20 ]; then
+    echo "${RED}TOO MUCH CHANGES \nREVERT AND COMMIT${NC}"
+
+else 
+    if [ $max -ge 10 ]; then
+        read -p "enter commit message or press ENTER to abort commit: " message
+        if [ -z "${message}" ]; then
+            echo "${GREEN}ABORT COMMIT${NC}"
+        
+        else
+            git add .
+            git commit -m "$message"
+        fi
+    fi
+
+    echo "RUN TESTS HERE"
+
+fi
